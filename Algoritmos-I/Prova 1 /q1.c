6527107601bfc605e95a886882edc30f2984b821
igor <igorbernardorocha@gmail.com>
2025-11-25 19:16:13 -0300

    questoes

---
+++ Algoritmos-I/Prova 1 /q1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int n1,n2;
+    int soma = 0;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero:\n");
+    scanf("%d", &n2);
+
+    printf("O resultado da soma e: %d\n", soma = n1 + n2);
+    return 0;
+}
---
+++ Algoritmos-I/Prova 1 /q2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int n1,n2;
+    int subtracao = 0;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero:\n");
+    scanf("%d", &n2);
+
+    printf("O resultado da subtracao e: %d\n", subtracao = n1 - n2);
+    return 0;
+}
