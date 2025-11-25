b78df118ea6229c6cd76565094034b5ea7451879
igor <igorbernardorocha@gmail.com>
2025-11-25 19:21:17 -0300

    .

---
+++ Algoritmos-I/Prova 1 /q3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int n1,n2;
+    int multi = 0;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero:\n");
+    scanf("%d", &n2);
+
+    printf("Resultado da subtracao e: %d\n", multi = n1 * n2);
+    return 0;
+}
